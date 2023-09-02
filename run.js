b64550e2617a2f3f6be0a1de84d775a881b1cd3e
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2023-09-02 16:59:09 -0300

    pulo
    feito o pulo

--- run.js
+++ run.js
@@ -1,4 +1,25 @@
-let inicio = document.getElementById("inicio")
+let inicio = document.getElementById("inicio");
+let jogo = document.getElementById("jogo")
+let tela = document.getElementById("telapeq")
+document.addEventListener('keydown', function(e){
+    if(e.key == "Enter") {
+        const btn = document.getElementById("btn");
+        btn.click();
+    }
+    if(e.key == " ") {
+        const btn = document.getElementById("btn");
+        btn.click();
+        pulo();
+    }
+})
 function jog(){
     inicio.style.display = 'none';
+    jogo.style.display = 'grid';
+}
+function pulo(){
+    tela.style.alignItems = "start"
+    setTimeout(volte, 2000)
+}
+function volte(){
+    tela.style.alignItems = "end"
 }
