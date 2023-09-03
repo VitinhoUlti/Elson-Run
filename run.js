9c5d64ca4094803352bc649c6082ebf27857f92a
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2023-09-02 21:17:19 -0300

    pontos
    pontos

--- run.js
+++ run.js
@@ -1,6 +1,9 @@
 let inicio = document.getElementById("inicio");
-let jogo = document.getElementById("jogo")
-let tela = document.getElementById("telapeq")
+let jogo = document.getElementById("jogo");
+let tela = document.getElementById("telapeq");
+let pontosf = document.getElementById("pontos");
+let pontosv = 0;
+
 document.addEventListener('keydown', function(e){
     if(e.key == "Enter") {
         const btn = document.getElementById("btn");
@@ -15,11 +18,16 @@ document.addEventListener('keydown', function(e){
 function jog(){
     inicio.style.display = 'none';
     jogo.style.display = 'grid';
+    function pontos(){
+    pontosv++;
+    pontosf.textContent = pontosv;
+}
+setInterval(pontos, 3000)
 }
 function pulo(){
-    tela.style.alignItems = "start"
+    tela.style.alignItems = "start";
     setTimeout(volte, 1000)
 }
 function volte(){
-    tela.style.alignItems = "end"
+    tela.style.alignItems = "end";
 }
