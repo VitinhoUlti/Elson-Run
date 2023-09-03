2e0a863c594da1cafa32ca0907ac7438057d7691
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2023-09-02 23:28:29 -0300

    amanhã
    ...

--- run.js
+++ run.js
@@ -1,8 +1,11 @@
 let inicio = document.getElementById("inicio");
 let jogo = document.getElementById("jogo");
+let bot = document.getElementById("bot")
+let jogador = document.getElementById("jogador")
 let tela = document.getElementById("telapeq");
 let pontosf = document.getElementById("pontos");
 let pontosv = 0;
+let vi = 0;
 
 document.addEventListener('keydown', function(e){
     if(e.key == "Enter") {
@@ -18,11 +21,15 @@ document.addEventListener('keydown', function(e){
 function jog(){
     inicio.style.display = 'none';
     jogo.style.display = 'grid';
-    function pontos(){
-    pontosv++;
-    pontosf.textContent = pontosv;
+    if(pontosv==0){
+        pontosv++
+        pontosf.textContent = pontosv;
+     function pontos(){
+     pontosv++;
+     pontosf.textContent = pontosv; 
+    }
 }
-setInterval(pontos, 3000)
+setInterval(pontos, 3*1000)
 }
 function pulo(){
     tela.style.alignItems = "start";
@@ -30,4 +37,10 @@ function pulo(){
 }
 function volte(){
     tela.style.alignItems = "end";
+}
+function gameover(){
+    if(bot.style.display == 'none'){
+        window.alert("punheta")
+    }
+    
 }
